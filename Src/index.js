465fe9f7d23f460645cc41fa994561a7dd996c2d
Sandip-dahal <sandipdahal141414@gmail.com>
2026-08-16 12:27:46 +0545

    api error and api response added

--- Src/index.js
+++ Src/index.js
@@ -7,6 +7,15 @@ dotenv.config({
 })
 
 connectDB()
+.then(()=>{
+    app.listen(process.env.PORT || 8000, () =>{
+        console.log(`server runinig in port ${process.env.PORT}`)
+    })
+
+})
+.catch((err) =>{
+    console.log("mongo db connection failed ...", err);
+})
 
 
 
